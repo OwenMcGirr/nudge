5d125abb9a557f46d8c3bfc7950067b9964b1dc2
Owen McGirr <o.a.mcgirr@gmail.com>
2026-04-13 15:54:23 +0100

    feat: add preload IPC bridge

---
+++ src/preload/index.ts
@@ -0,0 +1,10 @@
+import { contextBridge, ipcRenderer } from 'electron'
+
+contextBridge.exposeInMainWorld('electron', {
+  onShowSuggestion: (callback: (text: string) => void) => {
+    ipcRenderer.on('show-suggestion', (_event, text: string) => callback(text))
+  },
+  onHide: (callback: () => void) => {
+    ipcRenderer.on('hide-suggestion', () => callback())
+  }
+})
